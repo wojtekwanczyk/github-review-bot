de6847892d17937b3676319b67343e5a2802f60d
Wojtek Wanczyk <wwanczyk@cisco.com>
2024-05-15 12:08:16 +0200

    add webserver

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,33 @@
 package main
 
 import (
-	"github.com/wojtekwanczyk/github-review-bot/pkg/config"
-	"github.com/wojtekwanczyk/github-review-bot/pkg/webex"
+	"io"
+	"net/http"
+
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	webex.SendMessage("Test message", config.Webex.RoomID)
+	http.HandleFunc("/", Handler)
+	log.Fatal(http.ListenAndServe(":7001", nil))
+
+	// webex.SendMessage("Test message", config.Webex.RoomID)
+}
+
+type GithubData struct {
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	// decoder := json.NewDecoder(r.Body)
+	// var gd GithubData
+	// err := decoder.Decode(&gd)
+	// if err != nil {
+	// 	log.Errorf("Cannot decode request: %s", err)
+	// }
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Errorf("%s", err)
+	}
+	log.Info("Request data: %s", body)
 }
